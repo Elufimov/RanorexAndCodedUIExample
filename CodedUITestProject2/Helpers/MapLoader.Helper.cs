a15d93c63358d04d588ed412caab51088b7e14c0
Elufimov Michael <elufimov@gmail.com>
2012-11-07 14:05:11 +0400

    initial commit

---
+++ CodedUITestProject2/Helpers/MapLoader.Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Example1.Repositories;
+
+namespace Example1.Helpers.Loader
+{
+    public static class MapLoader
+    {
+        private static Calculator _calculator;
+
+        public static Calculator Calculator
+        {
+            get
+            {
+                if (_calculator == null)
+                    _calculator = new Calculator();
+                return _calculator;
+            }
+        }
+    }
+}
---
+++ CodedUITestProject2/Tests/CodedUITest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+
+using Ranorex;
+
+using Example1.Helpers.Loader;
+
+
+namespace Example1.Tests
+{
+    [CodedUITest]
+    public class CodedUITest1
+    {
+        public CodedUITest1()
+        {
+            
+        }
+
+        [TestMethod]
+        public void CodedUITestMethod1()
+        {
+            MapLoader.Calculator.MainForm.Calculator.N1Button.Click();
+            MapLoader.Calculator.MainForm.Calculator.AdditionButton.Click();
+            MapLoader.Calculator.MainForm.Calculator.N2Button.Click();
+            MapLoader.Calculator.MainForm.Calculator.EquallyButton.Click();
+            Assert.AreEqual("3", MapLoader.Calculator.MainForm.Calculator.ResultInput.TextValue);
+        }
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            Process.Start("calc.exe");
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            Host.Local.KillApplications("calc.exe");
+        }
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+    }
+}
